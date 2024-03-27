681922ef8577c5e76b70684784b182b675c6dc36
Iskandar <isikjandev@gmail.com>
2024-03-27 05:33:59 +0500

    100%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,8 @@
 $(document).ready(function () {
     $('#export-btn').click(function () {
         let table = document.getElementById('data-table');
-        let rows = table.getElementsByTagName('tr');
-        let data = [];
-        for (let i = 0; i < rows.length; i++) {
-            let row = [], cols = rows[i].querySelectorAll('td, th');
-            for (let j = 0; j < cols.length; j++) {
-                row.push(cols[j].innerText);
-            }
-            data.push(row.join('\t'));
-        }
-        let excel = data.join('\n');
-        let blob = new Blob([excel], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8' });
-        saveAs(blob, 'data.xlsx');
+        let wb = XLSX.utils.table_to_book(table, { sheet: 'SheetJS' });
+        XLSX.writeFile(wb, 'data.xlsx');
     });
 
     $('#copy-btn').click(function () {
